250f1cbeb11de93fb9c2de407cb8160d9219bc0b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-03-29 02:24:56 +0900

    version 0.0.11

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -40,7 +40,7 @@ class custom_build(build):
 
 setup(
     name='proofofwork',
-    version='0.0.9',
+    version='0.0.11',
     description='Simple hash-mining library',
     author='Kimiyuki Onaka',
     author_email='kimiyuki95@gmail.com',
